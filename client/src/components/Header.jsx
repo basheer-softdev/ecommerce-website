6e986b8f44d659ca4c8d20298c3cf84ffac5cd4e
BasheerMohamed <Basheer_Mohamed@proton.me>
2025-05-07 16:55:49 +0530

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="container mx-auto">
           <div className="flex items-center justify-between">
             <div className="w-1/2">
-              <p className="text-sm font-medium">
+              <p className="text-sm font-secondary">
                 Get up to 50% off new season styles, limited time only
               </p>
             </div>
@@ -18,15 +18,16 @@ const Header = () => {
                 <li>
                   <Link
                     to="/help-center"
-                    className="text-xs font-medium transition-colors duration-500 hover:text-purple-600"
+                    className="text-sm transition-colors duration-500 hover:text-purple-800"
                   >
                     Help Center
                   </Link>
                 </li>
+                <li>|</li>
                 <li>
                   <Link
                     to="/order-tracking"
-                    className="text-xs font-medium transition-colors duration-500 hover:text-purple-600"
+                    className="text-sm transition-colors duration-500 hover:text-purple-800"
                   >
                     Order Tracking
                   </Link>
@@ -39,14 +40,32 @@ const Header = () => {
       <div className="py-3">
         <div className="container mx-auto flex items-center justify-between">
           <div className="w-1/4">
-            <Link to="/">
-              <img src="/logo.jpg" alt="" />
-            </Link>
+            <Link to="/" className="!font-primary text-3xl">SHOP.CO</Link>
           </div>
           <div className="w-[45%]">
             <Search />
           </div>
-          <div className="w-[30%]"></div>
+          <div className="w-[30%] flex items-center">
+            <ul className="flex items-center gap-2 pl-7">
+              <li>
+                <Link
+                  to="/login"
+                  className="hover:text-purple-800 text-sm transition-colors duration-500"
+                >
+                  Login
+                </Link>
+              </li>
+              <li className="text-gray-500">/</li>
+              <li>
+                <Link
+                  to="/register"
+                  className="hover:text-purple-800 text-sm transition-colors duration-500"
+                >
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
